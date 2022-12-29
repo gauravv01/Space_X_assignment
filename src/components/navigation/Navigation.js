31f909765faa9d6c0346716171bee243f6d1c15a
gauravv01 <vermagauravverma841@gmail.com>
2022-12-29 22:55:39 +0530

    hamburger-added

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,25 +1,51 @@
 import { useNavigate } from 'react-router-dom';
+import {  useState } from 'react';
 import constants from '../../data/constants';
 import styles from './Navigation.module.css'
 
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const [Icon,setIcon]= useState(false);
+  const [Options,setOptions]= useState(true);
+
+  const placeIcon=()=>{
+if(window.innerWidth<='640'){
+  setIcon(true)
+  setOptions(false)
+}
+else if(window.innerWidth>'640'){
+  setIcon(false)
+  setOptions(true)
+}
+  }
+  window.addEventListener('load',placeIcon);
+  window.addEventListener('resize',placeIcon);
 
   const homepage = (e) => {
     navigate(constants.HOME_PAGE);
+    setIcon(true)
   };
   const payloadpage = (e) => {
     navigate(constants.PAYLOAD_PAGE);
+    setIcon(true)
   };
 
   return (
     <div className={styles.navbar}>
+      <div className={styles.outer_div}>
 <div className={styles.logo}>{constants.SPACEX}</div>
-<div className={styles.options}>
+{Options && <div className={styles.options}>
 <button onClick={homepage}>{constants.HISTORY}</button>
 <button  onClick={payloadpage} >{constants.PAYLOADS}</button>
+</div>}
 </div>
+{Icon && <div className={styles.hamburger} onClick={()=>setOptions(option=>!option)}>{constants.HAMBURGER.map(itm=>{return <span key={itm} className={styles.span}></span>})}</div>}
+{/* {Options && <div className={styles.options}>
+<button onClick={homepage}>{constants.HISTORY}</button>
+<button  onClick={payloadpage} >{constants.PAYLOADS}</button>
+</div>} */}
+
     </div>
   );
 };
